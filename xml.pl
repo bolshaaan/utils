da3c7b472e28b37c51317b687d6ab6c4eb6e7308
Bolshakov Alexander <bolshaaan@gmail.com>
2014-07-31 22:54:47 +0400

    add collecting 2 category tables

--- xml.pl
+++ xml.pl
@@ -3,14 +3,17 @@
 
 use strict;
 use warnings;
-
+use DBI;
 use utf8;
 
 use XML::Simple;
 
+use Encode qw(decode_utf8 encode_utf8);
 
 my $file = shift @ARGV;
 
+
+my $dbh = _connect();
 my $ref = XMLin($file)->{item};
 
 my $cats = {};
@@ -30,8 +33,71 @@ foreach my  $t (values %$ref) {
 #	print  $t->{rubric1} . " |  " . $t->{rubric2} . " |  " .  $t->{ rubric3 } . "\n";
 }
 
+
+
+my @ins_cat_desc = ();
+my @ins_cat = ();
+my $i = 0;
+sub _rc {
+	my $parent = shift;
+	my $h = shift;
+
+	foreach my $k (keys %$h) {
+		++$i;
+		print sprintf("%s\n", $k);
+		print sprintf("id= %s, parent = %s\n", $i, $parent);
+
+		push(@ins_cat_desc, "(" .   join(',',  $i, $dbh->quote( encode_utf8( $k ) ))   .")" );
+		push(@ins_cat, "(" .   join(',', $i, $parent) . ")" );
+		_rc($i, $h->{ $k });
+	}
+
+	return;
+}
+
+
+
+_rc(0, $cats);
+
+
+
 use Data::Dumper;
-$Data::Dumper::AutoEncode::ENCODING = 'utf8';
-print Dumper $cats;
+print Dumper \@ins_cat_desc;
+print Dumper \@ins_cat;
 
 
+$dbh->do("truncate table oc_category");
+
+$dbh->do("
+	insert into
+		oc_category(
+			category_id,
+			parent_id
+		)
+	values
+		" .  join(',' , @ins_cat )  . "
+");
+
+
+
+$dbh->do("truncate table oc_category_description");
+
+$dbh->do("
+	insert into
+		oc_category_description(
+			category_id,
+			name
+		)
+	values
+		" .  join(',' , @ins_cat_desc )  . "
+");
+
+
+
+sub _connect {
+
+	my $dbh = DBI->connect(  "DBI:mysql:database=store;host=localhost", 'root', '', { RaiseError => 1, mysql_enable_utf8 => 1 } );
+
+	return $dbh;
+
+}
